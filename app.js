38163f17bc3939d6e322150c4ef9fc9fad532db6
theericzhang <56938909+theericzhang@users.noreply.github.com>
2020-04-29 23:26:03 -0700

    accessibility considerations, tabbing key
    tab key returns focus with outline state?

--- app.js
+++ app.js
@@ -19,5 +19,13 @@ function myFunction() {
   }
 }
 
+function handleFirstTab(e) {
+    if (e.keyCode === 9) { // the "I am a keyboard user" key
+        document.body.classList.add('user-is-tabbing');
+        window.removeEventListener('keydown', handleFirstTab);
+    }
+}
+
+window.addEventListener('keydown', handleFirstTab);
 
 
