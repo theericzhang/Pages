a3852b6146e73889ae6ceca0caf94412867a9b4b
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2020-05-29 00:03:57 -0700

    aboutme, routed new page.
    May branch for following commits - SVG editing to allow for in-between transitions

--- app.js
+++ app.js
@@ -1,28 +1,39 @@
 const menuBtn = document.querySelector('.menu-btn');
+const subNav = document.querySelector('.overlay a');
+
+subNav.addEventListener('click', () => {
+    closeOverlay();
+})
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
 	if(!menuOpen){
-		menuBtn.classList.add('open');
-    document.getElementById("myNav").style.height = "100%";
-    document.getElementById("myNav").style.backgroundColor = "rgba(0,0,0,1)"
-    document.getElementById("navbar").style.backgroundColor = "rgba(0,0,0,1)"
-    document.getElementById("ericZ").style.color = "#fff";
-    document.documentElement.style.overflow = 'hidden';
-    document.body.scroll = "no";
-		menuOpen = true;
+		openOverlay();
 	}else{
-		menuBtn.classList.remove('open');
+		closeOverlay();
+	}
+})
+
+function closeOverlay(){
+    menuBtn.classList.remove('open');
     document.getElementById("myNav").style.height = "0%";
     document.getElementById("myNav").style.backgroundColor = "#FFF"
     document.getElementById("navbar").style.backgroundColor = "#FFF"
     document.getElementById("ericZ").style.color = "#000";
     document.documentElement.style.overflow = 'scroll';
     document.body.scroll = "yes";
+    menuOpen = false;
+}
 
-    // menuBtn.classList.
-		menuOpen = false;
-	}
-})
+function openOverlay(){
+    menuBtn.classList.add('open');
+    document.getElementById("myNav").style.height = "100%";
+    document.getElementById("myNav").style.backgroundColor = "rgba(0,0,0,1)"
+    document.getElementById("navbar").style.backgroundColor = "rgba(0,0,0,1)"
+    document.getElementById("ericZ").style.color = "#fff";
+    document.documentElement.style.overflow = 'hidden';
+    document.body.scroll = "no";
+    menuOpen = true;
+}
 
 var prevScrollpos = window.pageYOffset;
 
